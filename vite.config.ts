bd94fd192ef7b3c55bfab60a1ee821a4b8ea8fa9
SwapnilSkr <swapnilmkab@gmail.com>
2026-04-06 21:11:26 +0530

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     tailwindcss(),
     react(),
